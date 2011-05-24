4081b3458b2a5ea26664da1f8c8ac76df6d9296a
Greg Luck <gluck@gregluck.com>
2011-05-24 16:31:59 +1000

    Initial RI

---
+++ src/main/java/javax/cache/App.java
@@ -0,0 +1,13 @@
+package javax.cache;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
