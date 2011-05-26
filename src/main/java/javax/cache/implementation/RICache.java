ee66af6b6ee99922dd1aff730fbbc0d6e068891e
Yannis Cosmadopoulos <yannisc@gmail.com>
2011-05-26 14:33:58 -0700

    Further development of RI/TCK

--- src/main/java/javax/cache/implementation/RICache.java
+++ src/main/java/javax/cache/implementation/RICache.java
@@ -10,272 +10,186 @@ import java.util.Collection;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.Future;
 
 /**
  * RI
  */
 public class RICache<K,V> implements Cache<K,V> {
-    private final HashMap<K,V> store = new HashMap<K,V>();
+    private final ConcurrentHashMap<K,V> store = new ConcurrentHashMap<K,V>();
 
     /**
-     * Gets an entry from the cache.
-     * <p/>
-     * A return value of
-     * {@code null} does not <i>necessarily</i> indicate that the map
-     * contains no mapping for the key; it's also possible that the map
-     * explicitly maps the key to {@code null}.  The {@link #containsKey(Object)}
-     * operation may be used to distinguish these two cases.
-     *
-     * @param key the key whose associated value is to be returned
-     * @return the element, or null, if it does not exist.
-     * @throws IllegalStateException      if the cache is not {@link javax.cache.Status#STARTED}
-     * @throws IllegalArgumentException if the key is null
-     * @throws javax.cache.CacheException
+     * {@inheritDoc}
      */
     public V get(Object key) throws CacheException {
         if (key == null) {
-            throw new IllegalArgumentException("null key");
+            throw new NullPointerException("key");
         }
         return store.get(key);
     }
 
     /**
-     * The getAll method will return, from the cache, a Map of the objects
-     * associated with the Collection of keys in argument "keys". If the objects
-     * are not in the cache, the associated cache loader will be called. If no
-     * loader is associated with an object, a null is returned.  If a problem
-     * is encountered during the retrieving or loading of the objects, an
-     * exception will be thrown.
-     * <p/>
-     * If the "arg" argument is set, the arg object will be passed to the
-     * CacheLoader.loadAll method.  The cache will not dereference the object.
-     * If no "arg" value is provided a null will be passed to the loadAll
-     * method.
-     * <p/>
-     * The storing of null values in the cache is permitted, however, the get
-     * method will not distinguish returning a null stored in the cache and
-     * not finding the object in the cache. In both cases a null is returned.
-     *
-     * @param keys The keys whose associated values are to be returned.
-     * @return The entries for the specified keys.
+     * {@inheritDoc}
      */
     public Map<K, V> getAll(Collection<? extends K> keys) {
-        throw new UnsupportedOperationException();
+         // will throw NPE if keys=null
+        HashMap<K,V> map = new HashMap<K,V>(keys.size());
+        for (K key : keys) {
+            map.put(key, store.get(key));
+        }
+        return map;
     }
 
     /**
-     * Returns <tt>true</tt> if this cache contains a mapping for the specified
-     * key.  More formally, returns <tt>true</tt> if and only if
-     * this cache contains a mapping for a key <tt>k</tt> such that
-     * <tt>(key==null ? k==null : key.equals(k))</tt>.  (There can be
-     * at most one such mapping.)
-     * <p/>
-     *
-     * @param key key whose presence in this cache is to be tested.
-     *            null is permitted but the cache will always return null
-     * @return <tt>true</tt> if this map contains a mapping for the specified key
+     * {@inheritDoc}
      */
     public boolean containsKey(Object key) {
-        throw new UnsupportedOperationException();
+        if (key == null) {
+            throw new NullPointerException("key");
+        }
+        return store.containsKey(key);
     }
 
     /**
-     * The load method provides a means to "pre load" the cache. This method
-     * will, asynchronously, load the specified object into the cache using
-     * the associated cacheloader. If the object already exists in the cache,
-     * no action is taken. If no loader is associated with the object, no object
-     * will be loaded into the cache.  If a problem is encountered during the
-     * retrieving or loading of the object, an exception should
-     * be logged.
-     * <p/>
-     * If the "arg" argument is set, the arg object will be passed to the
-     * CacheLoader.load method.  The cache will not dereference the object. If
-     * no "arg" value is provided a null will be passed to the load method.
-     * The storing of null values in the cache is permitted, however, the get
-     * method will not distinguish returning a null stored in the cache and not
-     * finding the object in the cache. In both cases a null is returned.
-     *
-     * @param key the key
-     * @param specificLoader a specific loader to use. If null the default loader is used.
-     * @param loaderArgument provision for additional parameters to be passed to the loader
-     * @return a Future which can be used to monitor execution
+     * {@inheritDoc}
      */
-    public Future load(K key, CacheLoader specificLoader, Object loaderArgument) {
+    public Future load(K key, CacheLoader<K,V> specificLoader, Object loaderArgument) {
         throw new UnsupportedOperationException();
     }
 
     /**
-     * The loadAll method provides a means to "pre load" objects into the cache.
-     * This method will, asynchronously, load the specified objects into the
-     * cache using the associated cache loader. If the an object already exists
-     * in the cache, no action is taken. If no loader is associated with the
-     * object, no object will be loaded into the cache.  If a problem is
-     * encountered during the retrieving or loading of the objects, an
-     * exception (to be defined) should be logged.
-     * <p/>
-     * The getAll method will return, from the cache, a Map of the objects
-     * associated with the Collection of keys in argument "keys". If the objects
-     * are not in the cache, the associated cache loader will be called. If no
-     * loader is associated with an object, a null is returned.  If a problem
-     * is encountered during the retrieving or loading of the objects, an
-     * exception (to be defined) will be thrown.
-     * <p/>
-     * If the "arg" argument is set, the arg object will be passed to the
-     * CacheLoader.loadAll method.  The cache will not dereference the object.
-     * If no "arg" value is provided a null will be passed to the loadAll
-     * method.
-     *
-     * @param keys the keys
-     * @param specificLoader a specific loader to use. If null the default loader is used.
-     * @param loaderArgument provision for additional parameters to be passed to the loader
-     * @return a Future which can be used to monitor execution
+     * {@inheritDoc}
      */
     public Future loadAll(Collection<? extends K> keys, CacheLoader specificLoader, Object loaderArgument) {
         throw new UnsupportedOperationException();
     }
 
     /**
-     * Returns the CacheEntry object associated with the object identified by
-     * "key". If the object is not in the cache a null is returned.
+     * {@inheritDoc}
      */
     public Entry<K,V> getCacheEntry(Object key) {
         throw new UnsupportedOperationException();
     }
 
     /**
-     * Returns the CacheStatistics object associated with the cache.
-     * May return null if the cache does not support statistics gathering.
+     * {@inheritDoc}
      */
     public CacheStatisticsMBean getCacheStatistics() {
         throw new UnsupportedOperationException();
     }
 
     /**
-     * Add a listener to the list of cache listeners
+     * {@inheritDoc}
      */
     public void addListener(CacheEntryListener listener) {
         throw new UnsupportedOperationException();
     }
 
     /**
-     * Remove a listener from the list of cache listeners
+     * {@inheritDoc}
      */
     public void removeListener(CacheEntryListener listener) {
         throw new UnsupportedOperationException();
     }
 
     /**
-     * NOTE: different return value
-     *
-     * @see java.util.Map#put(Object, Object)
+     * {@inheritDoc}
      */
     public void put(K key, V value) {
         if (key == null) {
-            throw new IllegalArgumentException("null key");
+            throw new NullPointerException("key");
         }
         if (value == null) {
-            throw new IllegalArgumentException("null value");
+            throw new NullPointerException("value");
         }
         store.put(key, value);
     }
 
     /**
-     * @see java.util.Map#putAll(java.util.Map)
+     * {@inheritDoc}
      */
     public void putAll(Map<? extends K, ? extends V> m) {
-        throw new UnsupportedOperationException();
+        if (m == null) {
+            throw new NullPointerException();
+        }
+        if (m.containsKey(null)) {
+            throw new NullPointerException();
+        }
+        if (m.containsValue(null)) {
+            throw new NullPointerException();
+        }
+        store.putAll(m);
     }
 
     /**
-     * NOTE: different return value
-     *
-     * @see java.util.concurrent.ConcurrentMap#putIfAbsent(Object, Object)
+     * {@inheritDoc}
      */
     public boolean putIfAbsent(K key, V value) {
         throw new UnsupportedOperationException();
     }
 
     /**
-     * NOTE: different return value
-     *
-     * @return returns false if there was no matching key
-     * @see java.util.Map#remove(Object)
+     * {@inheritDoc}
      */
     public boolean remove(Object key) {
         if (key == null) {
-            throw new IllegalArgumentException("null key");
+            throw new NullPointerException("key");
         }
         return (store.remove(key) != null);
     }
 
     /**
-     * Removes the entry for a key only if currently mapped to a given value.
-     * <p/>
-     * This is equivalent to
-     * <pre>
-     *   if (cache.containsKey(key) &amp;&amp; cache.get(key).equals(value)) {
-     *       cache.remove(key);
-     *       return true;
-     *   } else return false;</pre>
-     * except that the action is performed atomically.
-     *
-     * @param key key with which the specified value is associated
-     * @return <tt>true</tt> if the value was removed
-     * @throws UnsupportedOperationException if the <tt>remove</tt> operation
-     *                                       is not supported by this cache
-     * @throws ClassCastException            if the key or value is of an inappropriate
-     *                                       type for this cache (optional)
-     * @throws NullPointerException          if the specified key or value is null,
-     *                                       and this cache does not permit null keys or values (optional)
+     * {@inheritDoc}
      */
     public V getAndRemove(Object key) {
         throw new UnsupportedOperationException();
     }
 
     /**
-     * @see java.util.concurrent.ConcurrentMap#replace(Object, Object, Object)
+     * {@inheritDoc}
      */
     public boolean replace(K key, V oldValue, V newValue) {
         throw new UnsupportedOperationException();
     }
 
     /**
-     * @see java.util.concurrent.ConcurrentMap#replace(Object, Object)
+     * {@inheritDoc}
      */
     public boolean replace(K key, V value) {
         throw new UnsupportedOperationException();
     }
 
     /**
-     * @see java.util.concurrent.ConcurrentMap#replace(Object, Object)
+     * {@inheritDoc}
      */
     public V getAndReplace(K key, V value) {
         throw new UnsupportedOperationException();
     }
 
     /**
-     * Removes all of the mappings from this cache.
-     * The cache will be empty after this call returns.
-     * <p/>
-     * This is potentially an expensive operation.
-     * <p/>
+     * {@inheritDoc}
+     */
+    public void removeAll(Collection<? extends K> keys) {
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * {@inheritDoc}
      */
     public void removeAll() {
         throw new UnsupportedOperationException();
     }
 
     /**
-     * @return the CacheConfiguration, which is immutable
+     * {@inheritDoc}
      */
     public CacheConfiguration getConfiguration() {
         throw new UnsupportedOperationException();
     }
 
     /**
-     * Returns an iterator over a set of elements of type T.
-     *
-     * @return an Iterator.
+     * {@inheritDoc}
      */
     public Iterator<Entry<K, V>> iterator() {
         throw new UnsupportedOperationException();
---
+++ src/main/java/javax/cache/implementation/RICacheConfiguration.java
@@ -0,0 +1,8 @@
+package javax.cache.implementation;
+
+/**
+ * @author ycosmado
+ * @since 1.0
+ */
+public class RICacheConfiguration {
+}
