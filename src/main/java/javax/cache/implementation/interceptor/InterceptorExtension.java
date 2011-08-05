10a2a59362b49e7f182de6787ed34dcd2e885f7b
Pete Muir <pmuir@bleepbleep.org.uk>
2011-08-05 19:23:15 +0100

    Fix the RI to allow removing @InterceptorBinding

---
+++ src/main/java/javax/cache/implementation/interceptor/InterceptorExtension.java
@@ -0,0 +1,43 @@
+/**
+ *  Copyright 2011 Terracotta, Inc.
+ *  Copyright 2011 Oracle America Incorporated
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package javax.cache.implementation.interceptor;
+
+import javax.cache.interceptor.CacheRemoveAll;
+import javax.cache.interceptor.CacheRemoveEntry;
+import javax.cache.interceptor.CacheResult;
+import javax.enterprise.event.Observes;
+import javax.enterprise.inject.spi.BeforeBeanDiscovery;
+import javax.enterprise.inject.spi.Extension;
+
+/**
+ * 
+ * @author pmuir
+ *
+ */
+public class InterceptorExtension implements Extension {
+   
+   /**
+    * 
+    * @param event
+    */
+   void registerInterceptorBindings(@Observes BeforeBeanDiscovery event) {
+      event.addInterceptorBinding(CacheResult.class);
+      event.addInterceptorBinding(CacheRemoveEntry.class);
+      event.addInterceptorBinding(CacheRemoveAll.class);
+   }
+
+}
