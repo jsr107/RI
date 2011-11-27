32ef4941ba159450fad505bc4b95177508273204
Yannis Cosmadopoulos <yannisc@gmail.com>
2011-11-27 10:32:54 -0800

    Added a LockManager

--- cache-ri-impl/src/main/java/javax/cache/implementation/RICache.java
+++ cache-ri-impl/src/main/java/javax/cache/implementation/RICache.java
@@ -32,9 +32,11 @@ import java.util.Iterator;
 import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.Callable;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.CopyOnWriteArraySet;
 import java.util.concurrent.Future;
 import java.util.concurrent.FutureTask;
+import java.util.concurrent.locks.ReentrantLock;
 
 /**
  * The reference implementation for JSR107.
@@ -769,5 +771,76 @@ public final class RICache<K, V> extends AbstractCache<K, V> {
      */
     long getSize() {
         return store.size();
+    }
+
+    /**
+     * Simple lock management
+     * @param <K> the type of the object to be locked
+     * @author Yannis Cosmadopoulos
+     * @since 1.0
+     */
+    private static class LockManager<K> {
+        private final ConcurrentHashMap<K, ReentrantLock> locks = new ConcurrentHashMap<K, ReentrantLock>();
+        private final LockFactory lockFactory = new LockFactory();
+
+        /**
+         * Lock the object
+         * @param key the key
+         */
+        private void lock(K key) {
+            ReentrantLock lock = lockFactory.getLock();
+
+            while (true) {
+                ReentrantLock oldLock = locks.putIfAbsent(key, lock);
+                if (oldLock == null) {
+                    return;
+                }
+                // there was a lock
+                oldLock.lock();
+                // now we have it. Because of possibility that someone had it for remove,
+                // we don't re-use directly
+                lockFactory.release(lock);
+            }
+        }
+
+        /**
+         * Unlock the object
+         * @param key the object
+         */
+        private void unLock(K key) {
+            ReentrantLock lock = locks.remove(key);
+            assert lock != null && lock.isLocked() && lock.isHeldByCurrentThread();
+            lockFactory.release(lock);
+        }
+
+        /**
+         * Factory/pool
+         */
+        private static class LockFactory {
+            private static final int CAPACITY = 100;
+            private static final ArrayList<ReentrantLock> LOCKS = new ArrayList<ReentrantLock>(CAPACITY);
+
+            private ReentrantLock getLock() {
+                ReentrantLock qLock = null;
+                synchronized (LOCKS) {
+                    if (!LOCKS.isEmpty()) {
+                        qLock = LOCKS.remove(0);
+                    }
+                }
+
+                ReentrantLock lock = qLock != null ? qLock : new ReentrantLock();
+                lock.lock();
+                return lock;
+            }
+
+            private void release(ReentrantLock lock) {
+                lock.unlock();
+                synchronized (LOCKS) {
+                    if (LOCKS.size() <= CAPACITY) {
+                        LOCKS.add(lock);
+                    }
+                }
+            }
+        }
     }
 }
