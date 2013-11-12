9df67196f6985325488600a85a2a9136b943f48d
Brian Oliver <brian.oliver@oracle.com>
2013-11-11 21:16:46 -0500

    Configuration type corrections (missing generic parameters)

--- cache-ri-impl/src/main/java/org/jsr107/ri/RICache.java
+++ cache-ri-impl/src/main/java/org/jsr107/ri/RICache.java
@@ -379,8 +379,8 @@ public final class RICache<K, V> implements Cache<K, V> {
    * {@inheritDoc}
    */
   @Override
-  public <C extends Configuration> C getConfiguration(Class<C> clazz) {
-    if (clazz.isAssignableFrom((configuration).getClass())) {
+  public <C extends Configuration<K, V>> C getConfiguration(Class<C> clazz) {
+    if (clazz.isInstance(configuration)) {
       return clazz.cast(configuration);
     }
     throw new IllegalArgumentException("The configuration class " + clazz +
