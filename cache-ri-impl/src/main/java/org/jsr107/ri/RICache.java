7e1f83db87fac5915f728e197abcc682aa05d485
Greg Luck <gluck@gregluck.com>
2013-12-09 12:21:25 +1000

    Fix checkstyle error

--- cache-ri-impl/src/main/java/org/jsr107/ri/RICache.java
+++ cache-ri-impl/src/main/java/org/jsr107/ri/RICache.java
@@ -63,7 +63,6 @@ import java.util.Set;
 import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.FutureTask;
 import java.util.concurrent.TimeUnit;
 import java.util.logging.Level;
 import java.util.logging.Logger;
